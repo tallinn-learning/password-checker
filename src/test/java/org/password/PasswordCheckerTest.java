8c7c37889abf3f19df3eebf820bd3e2f5ded99d1
Marija <gestuverteja.marija@gmail.com>
2024-02-15 22:18:31 +0200

    initial commit

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,6 +1,8 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordCheckerTest {
@@ -9,4 +11,26 @@ public class PasswordCheckerTest {
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
          assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
     }
+    @Test
+    void shouldReturnFalseNotMeetingLengthAndAdditionalRequirements(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("Short"));
+    }
+    @Test
+    void shouldReturnFalseNotMeetingOnlyLengthRequirements() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Short1@"));
+    }
+
+    @Test
+    void shouldReturnFalseForMissingDigitalCharacters(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password@"));
+    }
+    @Test
+    void shouldReturnFalseForMissingSpecialCharacters() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password1"));
+    }
+    @Test
+    void shouldReturnFalseForMissingBothSpecialCharactersAndDigits() {
+        assertFalse(PasswordChecker.checkPasswordComplexity("Password"));
+    }
+
 }
