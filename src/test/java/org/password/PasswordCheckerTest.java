4035f946d9a95bff4ce43d19d9b53e92f4db59e3
emvladislav <em.vladichek@gmail.com>
2024-02-15 21:13:24 +0200

    Added 7 new unit tests, checklist.txt file with checklist table, maven compiler version changed to 21.

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -8,5 +8,34 @@ public class PasswordCheckerTest {
 
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialCharsEng() {
-         assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );}
+        assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+    }
+    @Test
+    void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialCharsRus() {
+        assertTrue( PasswordChecker.checkPasswordComplexity("Пароль1234!") );
+    }
+    @Test
+    void shouldReturnFalseForNotEnoughComplexPasswordWithMissingNumber() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Password@@@") );
+    }
+    @Test
+    void shouldReturnFalseForNotEnoughComplexPasswordWithMissingSpecialChar() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("Password2213") );
+    }
+    @Test
+    void shouldReturnFalseForNotEnoughComplexPasswordNotEnoughLong() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("P2@") );
+    }
+    @Test
+    void shouldReturnFalsePasswordInputIsEmpty() {
+        assertFalse( PasswordChecker.checkPasswordComplexity("") );
+    }
+    @Test
+    void shouldReturnTrueForComplexPasswordWithOnlyDigitsAndSpecialChars() {
+        assertTrue( PasswordChecker.checkPasswordComplexity("123123@@@@") );
+    }
+    @Test
+    void shouldReturnTrueForComplexPasswordWithSpacesNumbersSpecialChars() {
+        assertTrue( PasswordChecker.checkPasswordComplexity("pa ss word123@") );
+    }
 }
