ca37a02d388e8a52e28cc33eeed72b9dee7105c2
Marija <gestuverteja.marija@gmail.com>
2024-02-19 19:31:30 +0200

    committing with formated files

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -9,25 +9,29 @@ public class PasswordCheckerTest {
 
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
-         assertTrue( PasswordChecker.checkPasswordComplexity("Password123#") );
+        assertTrue(PasswordChecker.checkPasswordComplexity("Password123#"));
     }
+
     @Test
-    void shouldReturnFalseNotMeetingLengthAndAdditionalRequirements(){
+    void shouldReturnFalseNotMeetingLengthAndAdditionalRequirements() {
         assertFalse(PasswordChecker.checkPasswordComplexity("short"));
     }
+
     @Test
     void shouldReturnFalseNotMeetingOnlyLengthRequirements() {
         assertFalse(PasswordChecker.checkPasswordComplexity("Short1@"));
     }
 
     @Test
-    void shouldReturnFalseForMissingDigitalCharacters(){
+    void shouldReturnFalseForMissingDigitalCharacters() {
         assertFalse(PasswordChecker.checkPasswordComplexity("Password@"));
     }
+
     @Test
     void shouldReturnFalseForMissingSpecialCharacters() {
         assertFalse(PasswordChecker.checkPasswordComplexity("Password1"));
     }
+
     @Test
     void shouldReturnFalseForMissingBothSpecialCharactersAndDigits() {
         assertFalse(PasswordChecker.checkPasswordComplexity("Password"));
