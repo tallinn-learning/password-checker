d43d706dd9ef6fc15e4b90dadf4d1ca92e5a49d1
emvladislav <em.vladichek@gmail.com>
2024-02-27 20:44:21 +0200

    added junit-upiter-params dependency iin pom.xml, added a few parameterized tests for API get order by ID endpoint

--- src/test/java/api/RestApiMocked.java
+++ src/test/java/api/RestApiMocked.java
@@ -1,9 +1,12 @@
 package api;
 import static io.restassured.RestAssured.*;
-
+import org.apache.http.HttpStatus;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RestApiMocked {
     @BeforeAll
@@ -101,6 +104,53 @@ public class RestApiMocked {
                 .then()
                 .statusCode(404);
     }
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+    @ParameterizedTest
+    public void getOrderByIdAndResponseCodeIsOk(int id){
+        given()
+                .log()
+                .all()
+                .when()
+                .get("/test-orders/{id}", id)
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {1,5,9,10})
+    public void getOrdersByIdAndCheckResponseCodeIsOk(int orderId) {
+        int responseOrderId =
+        given().
+                log()
+                .all()
+                .when()
+                .get("/test-orders/" + orderId)
+//                .get("/test-orders/{orderId}", orderId)
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .extract()
+                .path("id");
+        Assertions.assertEquals(orderId,responseOrderId);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 11, 25})
+    public void getOrdersByInvalidIdAndCheckResponseCodeIsBadRequest(int InvalidOrderId) {
+        given().
+                log()
+                .all()
+                .when()
+//                .get("/test-orders/" + InvalidOrderId)
+                .get("/test-orders/{InvalidOrderId}", InvalidOrderId)
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
 
 
 }
