be3eb4c4c65ba4bf802ec53ddfccb62ed964a130
Bupy <bupysoft@gmail.com>
2024-03-01 17:06:43 +0200

    Code reformatted / Build code added

--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,12 +1,13 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordCheckerTest {
 
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
-         assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+        assertTrue(PasswordChecker.checkPasswordComplexity("Password123!"));
     }
 }
