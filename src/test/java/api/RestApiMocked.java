c7eae252f43fa3edd1d14852e0f0e814921ff87a
Marija <gestuverteja.marija@gmail.com>
2024-03-05 16:42:55 +0200

    9 homework after clarifying the test pursue, and editing status code with HttpStatus

--- src/test/java/api/RestApiMocked.java
+++ src/test/java/api/RestApiMocked.java
@@ -127,8 +127,8 @@ public class RestApiMocked {
                 .statusCode(200);
     }
 
-    // 9 homework DELETE method
-    // 9.1 Positive test with valid data
+    /*// 9 homework DELETE method
+    // 9.1 Positive test with valid data (with valid id which contains valid credentials)
     @Test
     public void deleteOrderByIdCheckResponseCodeIsOk() {
         given()
@@ -143,7 +143,7 @@ public class RestApiMocked {
                 .statusCode(204);
     }
 
-    // 9.2 Negative test with invalid data (400 status code, bad request)
+    // 9.2 Negative test with invalid data which contains incorrect credentials(400 status code, bad request)
     @Test
     public void deleteOrderByIdCheckResponseCodeIsBadRequest() {
         given()
@@ -158,7 +158,7 @@ public class RestApiMocked {
                 .statusCode(400);
     }
 
-    // Negative scenario for not supported requests (405 status code. Request is not supported by the target resource)
+    // Negative scenario for not supported requests, when slash and id value are missing (405 status code. Request is not supported by the target resource)
     @Test
     public void deleteOrderByIdCheckResponseCodeIsNotSupported() {
         given()
@@ -173,7 +173,7 @@ public class RestApiMocked {
                 .statusCode(405);
     }
 
-    // Negative scenario for the Order that was not found (404 Status code)
+    // Negative scenario for the Order that was not found, not stored in the database (404 Status code)
     @Test
     public void deleteOrderByIdCheckResponseCodeOrderIsNotFound() {
         given()
@@ -201,6 +201,80 @@ public class RestApiMocked {
                 .log()
                 .all()
                 .statusCode(401);
+    }*/
+    // 9.1.1 Positive test with valid data (with valid id which contains valid credentials) with HttpStatus
+    @Test
+    public void deleteOrderByIdCheckResponseHttpCodeIsOk() {
+        given()
+                .log()
+                .all()
+                .when()
+                .header("api_key", "1234567890123456")
+                .delete("/test-orders/5")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_NO_CONTENT);
+    }
+
+    // 9.2.1 Negative test with invalid data which contains incorrect credentials(400 status code, bad request) with HttpStatus
+    @Test
+    public void deleteOrderByIdCheckResponseHttpCodeIsBadRequest() {
+        given()
+                .log()
+                .all()
+                .when()
+                .header("api_key", "1234567890123456")
+                .delete("/test-orders/3fa85f64-5717-4562-b3fc-2c963f66afa6")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    // Negative scenario for not supported requests, when slash and id value are missing (405 status code. Request is not supported by the target resource) with HttpStatus
+    @Test
+    public void deleteOrderByIdCheckResponseHttpCodeIsNotSupported() {
+        given()
+                .log()
+                .all()
+                .when()
+                .header("api_key", "1234567890123456")
+                .delete("/test-orders")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED);
+    }
+
+    // Negative scenario for the Order that was not found, not stored in the database (404 Status code) with HttpStatus
+    @Test
+    public void deleteOrderByIdCheckResponseHttpCodeOrderIsNotFound() {
+        given()
+                .log()
+                .all()
+                .when()
+                .header("api_key", "1234567890123456")
+                .delete("/test-orders/001")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
+    // Negative scenario for the unauthorized user (401 Status code) with HttpStatus
+    @Test
+    public void deleteOrderByIdCheckResponseHttpCodeIsNotUnauthorized() {
+        given()
+                .log()
+                .all()
+                .when()
+                .header("api_key", "12345")
+                .delete("/test-orders/1")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
     }
 
 }
