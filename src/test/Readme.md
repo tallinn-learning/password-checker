0d1c7a81d1f8266a5e0f2b5b2482295dfbade15d
Aleksei <alexsnigur8@gmail.com>
2024-03-08 09:48:10 +0200

    Readme file: Added cases 1-4 PasswordCheckerTest: Created tests in accordance with readme file

---
+++ src/test/Readme.md
@@ -0,0 +1,6 @@
+| N | CASE                                | AUTOMATED Y/N | STATUS |
+|---|-------------------------------------|---------------|--------|
+| 1 | Empty password check                | Y             | Passed |
+| 2 | Password without INT-s              | Y             | Passed |
+| 3 | Password without special characters | Y             | Passed |
+| 4 | Short password                      | Y             | Passed |
--- src/test/java/org/password/PasswordCheckerTest.java
+++ src/test/java/org/password/PasswordCheckerTest.java
@@ -1,6 +1,8 @@
 package org.password;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordCheckerTest {
@@ -8,5 +10,25 @@ public class PasswordCheckerTest {
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
          assertTrue( PasswordChecker.checkPasswordComplexity("Password123!") );
+    }
+
+    @Test
+    void emptyPasswordFieldTest(){
+        assertFalse(PasswordChecker.checkPasswordComplexity(""));
+    }
+
+    @Test
+    void passwordWithoutInts(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("test!@#$"));
+    }
+
+    @Test
+    void passwordWithoutSpecialChars(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("test12345"));
+    }
+
+    @Test
+    void shortPasswordTest(){
+        assertFalse(PasswordChecker.checkPasswordComplexity("abc123!"));
     }
 }
